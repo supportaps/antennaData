012b87a39323bb956c88c41d74ff65b3db105d16
Jaroslav <>
2020-11-25 17:13:45 +0200

    added main

---
+++ main.py
@@ -0,0 +1,163 @@
+
+import pymssql
+import pymysql
+
+import config
+from read_directory import read_antenna_directory
+
+
+
+
+
+def get_data_from_wbts_nsn():
+    result_list_wbts = []
+    conn_db = pymysql.connect(host=config.host_mysql, user=config.user_mysql, password=config.password_mysql, db=config.db_mysql)
+    cursor = conn_db.cursor()
+    cursor.execute(config.query_wbts_nsn)
+    result_set_wbts = cursor.fetchall()
+    for row in result_set_wbts:
+        result_list_wbts.append(row)
+    cursor.close()
+
+    return result_list_wbts
+
+
+def get_data_from_wcel_nsn():
+    result_list_wcel = []
+    conn_db = pymysql.connect(host=config.host_mysql, user=config.user_mysql, password=config.password_mysql, db=config.db_mysql)
+    cursor = conn_db.cursor()
+    cursor.execute(config.query_wcel_nsn)
+    result_set_wcel = cursor.fetchall()
+    for row in result_set_wcel:
+        result_list_wcel.append(row)
+    cursor.close()
+
+    return result_set_wcel
+
+
+def add_bts_name_to_list_nsn(wcel_list, wbts_list):
+    result_list_mixed = []
+
+    #print(wbts_list)
+    # print(wcel_list)
+    for wcel in range(len(wcel_list)):
+        # for column in range(len(wcel_list[wcel])):
+        # print('wcel : ',wcel_list[wcel] )
+        for wbts in range(len(wbts_list)):
+           # print(wbts_list[wbts])
+            if wcel_list[wcel][2] == wbts_list[wbts][1] and wcel_list[wcel][5] == wbts_list[wbts][0]:
+                combined_tuple = wcel_list[wcel] + wbts_list[wbts]
+                result_list_mixed.append(combined_tuple)
+
+
+    return result_list_mixed
+
+
+
+
+
+
+
+
+
+
+def get_data_from_rpdb():
+    conn_db = pymssql.connect(host=config.host_mssql, user=config.user_mssql, password=config.password_mssql, database=config.db_mssql)
+    cursor = conn_db.cursor()
+    cursor.execute(config.query_rpdb_mentor)
+    result_list_rpdb = []
+    result_set = cursor.fetchall()
+    for row in result_set:
+        result_list_rpdb.append(row)
+    return result_list_rpdb
+    cursor.close()
+
+def combined_list_from_rpdb_and_oss_to_antennas_file(oss_list, rpdb_list):
+    result_list_rpdb_oss_data = []
+    #print(oss_list)
+    for oss_cell in range(len(oss_list)):
+        for rpdb_cell in range(len(rpdb_list)):
+            if oss_list[oss_cell][0] == rpdb_list[rpdb_cell][2] and oss_list[oss_cell][1] == rpdb_list[rpdb_cell][3]:
+                #print(rpdb_list[rpdb_cell], "------>>>>>", oss_list[oss_cell])
+                combined_tuple = rpdb_list[rpdb_cell] + oss_list[oss_cell]
+                result_list_rpdb_oss_data.append(combined_tuple)
+
+    return result_list_rpdb_oss_data
+
+
+if __name__ == '__main__':
+
+    get_data_from_wbts_nsn()
+    wcel_list = get_data_from_wcel_nsn()
+    wbts_list = get_data_from_wbts_nsn()
+    rpdb_list = get_data_from_rpdb()
+    oss_data_list = add_bts_name_to_list_nsn(wcel_list,wbts_list)
+    list_for_antenna_file = combined_list_from_rpdb_and_oss_to_antennas_file(oss_data_list, rpdb_list)
+
+    tech = 'UMTS'
+    equipment = 'RNC'
+    head = "Technology	RNC Name	RNC Id	NodeB Name	NodeB Id	ENB ID	NodeB Longitude	NodeB Latitude	Sector Name	Active	Noise Figure	AntennaID	Antenna Model	Sector Keywords	Antenna Longitude	Antenna Latitude	Height	Mechanical DownTilt	Azimuth	Downlink Loss	Uplink Loss	RTT fix A Coefficient	RTT fix B Coefficient	RET ID	In Building	Cable Lengths(Calculated)	Sector Height Level(Calculated)"
+    antenna_number = 0
+    with open("Antennas.txt", "w") as antennas_file:
+        antennas_file.write(head)
+        antennas_file.write('\n')
+        for row in range(len(list_for_antenna_file)):
+            #for el in range(len(list_for_antenna_file[row])):
+
+                antenna_number = int(list_for_antenna_file[row][16])
+                azimuth = int(list_for_antenna_file[row][10])
+                antenna_profile_from_rpdb = list_for_antenna_file[row][6]
+
+                if antenna_number == 1 or antenna_number == 2 or antenna_number == 3:
+                    antenna_number = 1
+                elif antenna_number == 4 or antenna_number == 5 or antenna_number == 6:
+                    antenna_number = 2
+                elif antenna_number == 7 or antenna_number == 8 or antenna_number == 9:
+                    antenna_number = 3
+                elif antenna_number == 10 or antenna_number == 11 or antenna_number == 12:
+                    antenna_number = 4
+                elif antenna_number == 13 or antenna_number == 14 or antenna_number == 15:
+                    antenna_number = 5
+                elif antenna_number == 16 or antenna_number == 17 or antenna_number == 18:
+                    antenna_number = 6
+
+                if azimuth == 360 or azimuth in range(859,999):
+                    azimuth = 0
+
+
+
+                antenna_directory_list = read_antenna_directory()
+                antenna_profile_name = antenna_profile_from_rpdb + '.txt'
+                for profile in range(len(antenna_directory_list)):
+                    for antenna in range(len(antenna_directory_list[profile])):
+                        if antenna_directory_list[profile][antenna] == antenna_profile_name:
+
+                            antenna_profile_from_rpdb = f"{antenna_directory_list[profile][0]}/{antenna_profile_from_rpdb}"
+
+                antennas_file.write(f"{tech}\t"
+                                    f"{equipment + str(list_for_antenna_file[row][19])}\t"
+                                    f"{str(list_for_antenna_file[row][19])}\t"
+                                    f"{str(list_for_antenna_file[row][22])}\t"
+                                    f"{str(list_for_antenna_file[row][19]) + '_' + str(list_for_antenna_file[row][20])}\t"
+                                    f"{'N/A'}\t"
+                                    f"{list_for_antenna_file[row][7]}\t"
+                                    f"{list_for_antenna_file[row][8]}\t"
+                                    f"{list_for_antenna_file[row][17]}\t"
+                                    f"{'true'}\t"
+                                    f"{'5.0'}\t"
+                                    f"{str(list_for_antenna_file[row][20]) + '/' + str(antenna_number)}\t"
+                                    f"{antenna_profile_from_rpdb}\t" #antenna directory
+                                    f"\t"
+                                    f"{list_for_antenna_file[row][7]}\t"
+                                    f"{list_for_antenna_file[row][8]}\t"
+                                    f"{str(list_for_antenna_file[row][9])}\t"
+                                    f"{'0.0'}\t"
+                                    f"{azimuth}\t"
+                                    f"{'3.0'}\t"
+                                    f"{3.0}\t"
+                                    f"{0.0}\t"
+                                    f"{0.0}\t"
+                                    f"\t"
+                                    f"{'false'}\t"
+                                    f"\t"
+                                    f"{'Overground'}\n")
